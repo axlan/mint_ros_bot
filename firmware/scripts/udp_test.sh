27cda37194d75d805462cccef3de07fd8b11d3d2
Jonathan Diamond <jonathan@pointonenav.com>
2025-08-18 14:34:23 -0700

    HAL working.

--- firmware/scripts/udp_test.sh
+++ firmware/scripts/udp_test.sh
@@ -3,7 +3,8 @@
 source /opt/ros/humble/setup.bash
 source ~/src/turtle_bot/microros_ws/install/local_setup.bash
 
-ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 --verbose 5
+ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888 --verbose 4
 
 # source /opt/ros/humble/setup.zsh
 # ros2 topic echo micro_ros_platformio_node_publisher
+#  ros2 topic echo /odom/unfiltered --field pose.pose
